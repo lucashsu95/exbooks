7f901a1e99f0295511e5e9f7211dc2c6591b0277
lucashsu95 <lucashsu95@gmail.com>
2026-03-14 21:45:41 +0800

    feat: 強化 OfficialBook 與 BookPhoto 的 Admin 管理介面
    - 新增書籍封面與書況照片預覽功能 (thumbnail)
    - 設定每頁顯示 20 筆資料 (list_per_page)
    - 使用 fieldsets 優化 OfficialBook 的表單欄位分組

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import BookPhoto, BookSet, OfficialBook, SharedBook, WishListItem
 
@@ -6,68 +7,124 @@ from .models import BookPhoto, BookSet, OfficialBook, SharedBook, WishListItem
 class BookPhotoInline(admin.TabularInline):
     model = BookPhoto
     extra = 0
-    fields = ('photo', 'caption', 'uploader', 'deal', 'created_at')
-    readonly_fields = ('created_at',)
-    autocomplete_fields = ('uploader',)
+    fields = ("photo", "caption", "uploader", "deal", "created_at")
+    readonly_fields = ("created_at",)
+    autocomplete_fields = ("uploader",)
 
 
 class SharedBookInline(admin.TabularInline):
     model = SharedBook
     extra = 0
-    fields = ('owner', 'keeper', 'status', 'transferability', 'loan_duration_days')
-    autocomplete_fields = ('owner', 'keeper')
+    fields = ("owner", "keeper", "status", "transferability", "loan_duration_days")
+    autocomplete_fields = ("owner", "keeper")
     show_change_link = True
 
 
 @admin.register(OfficialBook)
 class OfficialBookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'isbn', 'author', 'publisher', 'created_at')
-    search_fields = ('title', 'isbn', 'author', 'publisher')
-    list_filter = ('publisher',)
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = (
+        "title",
+        "isbn",
+        "author",
+        "publisher",
+        "cover_image_preview",
+        "created_at",
+    )
+    search_fields = ("title", "isbn", "author", "publisher")
+    list_filter = ("publisher",)
+    readonly_fields = ("cover_image_preview", "created_at", "updated_at")
+    list_per_page = 20
     inlines = [SharedBookInline]
+
+    fieldsets = (
+        ("基本資訊", {"fields": ("title", "isbn", "author", "publisher")}),
+        ("詳細資訊", {"fields": ("cover_image", "cover_image_preview", "description")}),
+        (
+            "系統資訊",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    @admin.display(description="封面預覽")
+    def cover_image_preview(self, obj):
+        if obj.cover_image:
+            return format_html(
+                '<img src="{}" style="max-height: 50px;" />', obj.cover_image.url
+            )
+        return "無圖片"
 
 
 @admin.register(BookSet)
 class BookSetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'created_at')
-    list_select_related = ('owner',)
-    search_fields = ('name', 'owner__username')
-    autocomplete_fields = ('owner',)
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("name", "owner", "created_at")
+    list_select_related = ("owner",)
+    search_fields = ("name", "owner__username")
+    autocomplete_fields = ("owner",)
+    readonly_fields = ("created_at", "updated_at")
 
 
 @admin.register(SharedBook)
 class SharedBookAdmin(admin.ModelAdmin):
     list_display = (
-        'official_book', 'owner', 'keeper', 'status',
-        'transferability', 'loan_duration_days', 'listed_at',
+        "official_book",
+        "owner",
+        "keeper",
+        "status",
+        "transferability",
+        "loan_duration_days",
+        "listed_at",
     )
-    list_select_related = ('official_book', 'owner', 'keeper', 'book_set')
-    list_filter = ('status', 'transferability')
+    list_select_related = ("official_book", "owner", "keeper", "book_set")
+    list_filter = ("status", "transferability")
     search_fields = (
-        'official_book__title', 'official_book__isbn',
-        'owner__username', 'keeper__username',
+        "official_book__title",
+        "official_book__isbn",
+        "owner__username",
+        "keeper__username",
     )
-    autocomplete_fields = ('official_book', 'owner', 'keeper', 'book_set')
-    readonly_fields = ('created_at', 'updated_at')
+    autocomplete_fields = ("official_book", "owner", "keeper", "book_set")
+    readonly_fields = ("created_at", "updated_at")
     inlines = [BookPhotoInline]
 
 
 @admin.register(BookPhoto)
 class BookPhotoAdmin(admin.ModelAdmin):
-    list_display = ('shared_book', 'uploader', 'caption', 'deal', 'created_at')
-    list_select_related = ('shared_book', 'shared_book__official_book', 'uploader', 'deal')
-    list_filter = ('created_at',)
-    search_fields = ('shared_book__official_book__title', 'caption')
-    autocomplete_fields = ('shared_book', 'uploader', 'deal')
-    readonly_fields = ('created_at',)
+    list_display = (
+        "shared_book",
+        "uploader",
+        "photo_preview",
+        "caption",
+        "deal",
+        "created_at",
+    )
+    list_select_related = (
+        "shared_book",
+        "shared_book__official_book",
+        "uploader",
+        "deal",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("shared_book__official_book__title", "caption")
+    autocomplete_fields = ("shared_book", "uploader", "deal")
+    readonly_fields = ("photo_preview", "created_at")
+    list_per_page = 20
+
+    @admin.display(description="照片預覽")
+    def photo_preview(self, obj):
+        if obj.photo:
+            return format_html(
+                '<img src="{}" style="max-height: 50px;" />', obj.photo.url
+            )
+        return "無圖片"
 
 
 @admin.register(WishListItem)
 class WishListItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'official_book', 'created_at')
-    list_select_related = ('user', 'official_book')
-    search_fields = ('user__username', 'official_book__title', 'official_book__isbn')
-    autocomplete_fields = ('user', 'official_book')
-    readonly_fields = ('created_at',)
+    list_display = ("user", "official_book", "created_at")
+    list_select_related = ("user", "official_book")
+    search_fields = ("user__username", "official_book__title", "official_book__isbn")
+    autocomplete_fields = ("user", "official_book")
+    readonly_fields = ("created_at",)
