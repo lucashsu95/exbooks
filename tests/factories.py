2516423f011471bcf3c3442e1bfc0196446c80da
lucashsu95 <lucashsu95@gmail.com>
2026-03-15 12:02:27 +0800

    feat(tests): 設定 Faker locale 為 zh_TW 產生繁體中文假資料
    - 在各個 Factory (User, OfficialBook, DealMessage, Rating 等) 的 Faker 中加上 `locale='zh_TW'`。
    - 覆寫 `UserProfileFactory._create` 以解決因 `post_save` signal 自動建立空白 profile 而導致 nickname 無法寫入的問題。

--- tests/factories.py
+++ tests/factories.py
@@ -10,33 +10,47 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Sequence(lambda n: f'user{n}')
-    email = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
-    password = factory.PostGenerationMethodCall('set_password', 'testpass123')
+    username = factory.Sequence(lambda n: f"user{n}")
+    email = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
+    first_name = factory.Faker("first_name", locale="zh_TW")
+    last_name = factory.Faker("last_name", locale="zh_TW")
+    password = factory.PostGenerationMethodCall("set_password", "testpass123")
 
 
 class UserProfileFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = UserProfile
-        django_get_or_create = ('user',)
+        django_get_or_create = ("user",)
 
     user = factory.SubFactory(UserFactory)
-    nickname = factory.Faker('name', locale='zh_TW')
+    nickname = factory.Faker("name", locale="zh_TW")
     default_transferability = UserProfile.Transferability.RETURN
-    default_location = '台北市信義區'
+    default_location = "台北市信義區"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Override to update the profile created by User post_save signal."""
+        user = kwargs.get("user")
+        if user:
+            # Profile might already exist due to signal
+            profile, created = model_class.objects.get_or_create(user=user)
+            # Update fields with factory generated values
+            for key, value in kwargs.items():
+                setattr(profile, key, value)
+            profile.save()
+            return profile
+        return super()._create(model_class, *args, **kwargs)
 
 
 class OfficialBookFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = OfficialBook
 
-    isbn = factory.Sequence(lambda n: f'{9780000000000 + n}')
-    title = factory.Faker('sentence', nb_words=4)
-    author = factory.Faker('name')
-    publisher = factory.Faker('company')
-    description = factory.Faker('paragraph')
+    isbn = factory.Sequence(lambda n: f"{9780000000000 + n}")
+    title = factory.Faker("sentence", nb_words=4, locale="zh_TW")
+    author = factory.Faker("name", locale="zh_TW")
+    publisher = factory.Faker("company", locale="zh_TW")
+    description = factory.Faker("paragraph", locale="zh_TW")
 
 
 class BookSetFactory(factory.django.DjangoModelFactory):
@@ -44,8 +58,8 @@ class BookSetFactory(factory.django.DjangoModelFactory):
         model = BookSet
 
     owner = factory.SubFactory(UserFactory)
-    name = factory.Sequence(lambda n: f'套書 {n}')
-    description = factory.Faker('sentence')
+    name = factory.Sequence(lambda n: f"套書 {n}")
+    description = factory.Faker("sentence", locale="zh_TW")
 
 
 class SharedBookFactory(factory.django.DjangoModelFactory):
@@ -59,7 +73,7 @@ class SharedBookFactory(factory.django.DjangoModelFactory):
     status = SharedBook.Status.SUSPENDED
     loan_duration_days = 30
     extend_duration_days = 14
-    condition_description = '書況良好'
+    condition_description = "書況良好"
 
 
 class BookPhotoFactory(factory.django.DjangoModelFactory):
@@ -68,8 +82,8 @@ class BookPhotoFactory(factory.django.DjangoModelFactory):
 
     shared_book = factory.SubFactory(SharedBookFactory)
     uploader = factory.LazyAttribute(lambda obj: obj.shared_book.owner)
-    photo = factory.django.ImageField(filename='test.jpg')
-    caption = '書況照片'
+    photo = factory.django.ImageField(filename="test.jpg")
+    caption = "書況照片"
 
 
 class WishListItemFactory(factory.django.DjangoModelFactory):
@@ -98,7 +112,7 @@ class DealMessageFactory(factory.django.DjangoModelFactory):
 
     deal = factory.SubFactory(DealFactory)
     sender = factory.LazyAttribute(lambda obj: obj.deal.applicant)
-    content = factory.Faker('paragraph')
+    content = factory.Faker("paragraph", locale="zh_TW")
 
 
 class RatingFactory(factory.django.DjangoModelFactory):
@@ -108,10 +122,10 @@ class RatingFactory(factory.django.DjangoModelFactory):
     deal = factory.SubFactory(DealFactory)
     rater = factory.LazyAttribute(lambda obj: obj.deal.applicant)
     ratee = factory.LazyAttribute(lambda obj: obj.deal.responder)
-    integrity_score = factory.Faker('random_int', min=1, max=5)
-    punctuality_score = factory.Faker('random_int', min=1, max=5)
-    accuracy_score = factory.Faker('random_int', min=1, max=5)
-    comment = factory.Faker('sentence')
+    integrity_score = factory.Faker("random_int", min=1, max=5)
+    punctuality_score = factory.Faker("random_int", min=1, max=5)
+    accuracy_score = factory.Faker("random_int", min=1, max=5)
+    comment = factory.Faker("sentence", locale="zh_TW")
 
 
 class LoanExtensionFactory(factory.django.DjangoModelFactory):
@@ -130,6 +144,6 @@ class NotificationFactory(factory.django.DjangoModelFactory):
 
     recipient = factory.SubFactory(UserFactory)
     notification_type = Notification.NotificationType.DEAL_REQUESTED
-    title = '測試通知'
-    message = '這是一則測試通知'
+    title = "測試通知"
+    message = "這是一則測試通知"
     is_read = False
